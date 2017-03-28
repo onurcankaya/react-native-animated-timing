3c463e2ec871cb978456d37885234228b2f801e5
Onur Çankaya <onur.eren.cankaya@gmail.com>
2017-03-28 09:20:28 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { AppRegistry, View, StyleSheet } from 'react-native';
+
+export default class AnimatedTiming extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.box} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  box: {
+    backgroundColor: "#333",
+    width: 100,
+    height: 100,
+  }
+})
+
+AppRegistry.registerComponent('AnimatedTiming', () => AnimatedTiming);
